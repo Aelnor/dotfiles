7a41a12d6af492e95c6d613b3796060c53a1d480
Alexey Chernyshov <eiden127@gmail.com>
2021-09-24 12:38:16 +0300

    cumulative changes for half a year

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,10 +3,12 @@
 # Terminate already running bar instances
 killall -q polybar
 
+MONITOR=`polybar -m|tail -1|sed -e 's/:.*$//g'`
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar main &
 
-echo "Polybar launched..."
+echo "Polybar launched on $MONITOR..."
