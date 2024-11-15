55f54d573e00db4bb1362d20e2ccdc1f674914c7
Alexey Chernyshov <eiden127@gmail.com>
2024-11-15 16:14:26 +0200

    incremental change once again

--- NeoVim/lua/plugins.lua
+++ NeoVim/lua/plugins.lua
@@ -399,25 +399,7 @@ require("lazy").setup({
 			})
 		end,
 	},
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-		}
-	},
+	"tpope/vim-fugitive",
 	{
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
@@ -434,88 +416,7 @@ require("lazy").setup({
 		lazy = false,
 	},
 	{
-		"epwalsh/obsidian.nvim",
-		version = "*",  -- recommended, use latest release instead of latest commit
-		lazy = true,
-		ft = "markdown",
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		-- event = {
-			--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-			--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-			--   "BufReadPre path/to/my-vault/**.md",
-			--   "BufNewFile path/to/my-vault/**.md",
-			-- },
-		dependencies = {
-			-- Required.
-			"nvim-lua/plenary.nvim",
-		},	
-		opts = {
-			workspaces = {
-				{
-					name = "vault",
-					path = "~/Documents/Vault",
-				},
-			},
-			notes_subdir = "Inbox",
-			new_notes_location = "notes_subdir",
-			-- Optional, customize how note IDs are generated given an optional title.
-			---@param title string|?
-			---@return string
-			note_id_func = function(title)
-				-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-				-- In this case a note with the title 'My new note' will be given an ID that looks
-				-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-				local suffix = ""
-				if title ~= nil then
-					-- If title is given, transform it into valid file name.
-					suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-				else
-					-- If title is nil, just add 4 random uppercase letters to the suffix.
-					for _ = 1, 4 do
-						suffix = suffix .. string.char(math.random(65, 90))
-					end
-				end
-				return tostring(os.time()) .. "-" .. suffix
-			end,
-
-			-- Optional, customize how note file names are generated given the ID, target directory, and title.
-			---@param spec { id: string, dir: obsidian.Path, title: string|? }
-			---@return string|obsidian.Path The full path to the new note.
-			note_path_func = function(spec)
-				-- This is equivalent to the default behavior.
-				local path = spec.dir / tostring(spec.id)
-				return path:with_suffix(".md")
-			end,
-
-			templates = {
-				folder = "Templates",
-				date_format = "%Y-%m-%d-%a",
-				time_format = "%H:%M",
-			},
-			daily_notes = {
-				-- Optional, if you keep daily notes in a separate directory.
-				folder = "Periodic",
-				-- Optional, if you want to change the date format for the ID of daily notes.
-				date_format = "%Y/%m/%d-%b-%Y",
-				-- Optional, if you want to change the date format of the default alias of daily notes.
-				alias_format = "%B %-d, %Y",
-				-- Optional, default tags to add to each new daily note created.
-				default_tags = { "daily-notes" },
-				-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-				template = "Daily.md"
-			},
-			-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-			completion = {
-				-- Set to false to disable completion.
-				nvim_cmp = true,
-				-- Trigger completion at 2 chars.
-				min_chars = 2,
-			}
-		},
-		keys = {
-			{ "<leader>nt", "<cmd>ObsidianToday<cr>", desc = "Open today daily note in Obsidian Vault" },
-			{ "<leader>nn", "<cmd>ObsidianNew<cr>", desc = "Create new note in Obsidian Vault" }
-		}
+		"rest-nvim/rest.nvim",
 	}
 })
 
